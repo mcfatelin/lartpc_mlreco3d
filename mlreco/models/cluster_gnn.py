0bca601ffd62ba1299759821527752962ed9b0e6
mcfatelin <mcfatelin@gmail.com>
2020-01-30 14:07:14 +0100

    Bug fix

--- mlreco/models/cluster_gnn.py
+++ mlreco/models/cluster_gnn.py
@@ -244,7 +244,9 @@ class EdgeChannelLoss(torch.nn.Module):
 
             # Increment the loss, balance classes if requested
             if self.balance_classes:
-                counts = torch.unique(edge_assn, return_counts=True)[1]
+                counts = torch.tensor([0,0])
+                val, Counts = torch.unique(edge_assn, return_counts=True)[1]
+                counts[val] = Counts
                 weights = np.array([float(counts[k])/len(edge_assn) for k in range(2)])
                 for k in range(2):
                     total_loss += (1./weights[k])*self.lossfn(edge_pred[edge_assn==k], edge_assn[edge_assn==k])
