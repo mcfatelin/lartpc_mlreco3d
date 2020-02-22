c2d6f0676b5c7828f4c2ae30d2de3d84bcaed92a
mcfatelin <mcfatelin@gmail.com>
2020-02-21 19:26:32 -0800

    GAN (not finished)

--- mlreco/models/gan.py
+++ mlreco/models/gan.py
@@ -33,18 +33,45 @@ class GAN(torch.nn.Module):
     def forward(self, input):
         """
         Args:
-            data ([torch.tensor]): (N, >=6) [x, y, z, batchid, value, label (binary)]
+            data ([torch.tensor]): (N, >=6) [x, y, z, batchid, value, label (binary)]. Label is "simulation" (1) or "data" (0). Label need to binary at the moment.
         Returns:
             dict:
                 'generated_data': (N, >=6) [x, y, z, batchid, value, label (binary)] if label=0, return the same data as input
-                'label_pred': predicted label by discriminator
+                'label_pred_raw': predicted label by discriminator for original data.
+                'batch_id_raw': The corresponding batch ids for label_pred_raw
+                'label_pred_gen': predicted label by discriminator for generated data.
+                'batch_id_gen': The corresponding batch ids for label_pred_gen. There can be batch ids missing, meaning they are simulations.
         """
         # Get the input
         data = input[0]
         device = data.device
 
-        # Get the image and label
-        image = data[:,:5] # [tensor]: (N,5) -> [x,y,z,batchid,value]
-        label = data[:,5] # label whether it is "simulation" (0) or "data" (1)
+        # Get the indexes for data
+        inds_data = data[:,5].nonzero().view(-1)
 
-        #
+        # Get the indexes for simulation
+        inds_sim  = (data[:,5]==1).nonzero().view(-1)
+
+        # Get the generated data
+        generated_data = self.generator(data[inds_data,:])
+
+        # Get the prediction of label
+        label_pred_raw, batch_id_raw = self.discriminator(data)
+        label_pred_gen, batch_id_gen = self.discriminator(generated_data)
+
+        # concatenate the data
+        generated_data = torch.concat((data[inds_sim,:], generated_data),dim=0)
+
+        return {
+            'generated_data': [generated_data],
+            'label_pred_raw': [label_pred_raw],
+            'label_pred_gen': [label_pred_gen],
+            'batch_id_raw':   [batch_id_raw],
+            'batch_id_gen':   [batch_id_gen],
+        }
+
+
+class GANLoss(torch.nn.Module):
+    """
+    Take the output of GAN and computes the loss
+    """
