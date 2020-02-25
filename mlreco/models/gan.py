6eed94567ce68a273c6ff7194a03345015a6c293
mcfatelin <mcfatelin@gmail.com>
2020-02-24 17:52:54 -0800

    GAN daily save

--- mlreco/models/gan.py
+++ mlreco/models/gan.py
@@ -74,4 +74,53 @@ class GAN(torch.nn.Module):
 class GANLoss(torch.nn.Module):
     """
     Take the output of GAN and computes the loss
+
+    for use in config:
+    model:
+        name: gan
+        modules:
+            loss            : <loss function: 'CE' or 'MM' (default 'CE')>
+            reduction       : <loss reduction method: 'mean' or 'sum' (default 'sum')>
+            gen_loss_factor : <factor before the pixel-wise loss for generator, default None>
+            generator:
+                <uresnet config>
+            discriminator:
+                <cnn_encoder config>
     """
+    def __init__(self, cfg):
+        super(GANLoss, self).__init__()
+
+        # Get the chain input parameters
+        chain_config = cfg['chain']
+
+        # Set the loss
+        self.loss = chain_config.get('loss', 'CE')
+        self.reduction = chain_config.get('reduction', 'mean')
+
+        if self.loss == 'CE':
+            self.lossfn = torch.nn.CrossEntropyLoss(reduction=self.reduction)
+        elif self.loss == 'MM':
+            p = chain_config.get('p', 1)
+            margin = chain_config.get('margin', 1.0)
+            self.lossfn = torch.nn.MultiMarginLoss(p=p, margin=margin, reduction=self.reduction)
+        else:
+            raise Exception('Loss not recognized: ' + self.loss)
+
+
+    def forward(self, out, clusters):
+        """
+        Applies the requested loss for gan
+        The goal is to discriminate between data and simulation as much as possible
+        In the meantime can label the generated data as data as much as possible
+
+        Args:
+            out (dict):
+                'generated_data': (N, >=6) [x, y, z, batchid, value, label (binary)] if label=0, return the same data as input
+                'label_pred_raw': predicted label by discriminator for original data.
+                'batch_id_raw': The corresponding batch ids for label_pred_raw
+                'label_pred_gen': predicted label by discriminator for generated data.
+                'batch_id_gen': The corresponding batch ids for label_pred_gen. There can be batch ids missing, meaning they are simulations.
+            clusters ([torch.tensor])     : (N,>=6) [x, y, z, batchid, value, label]
+        Returns:
+            double: loss, accuracy
+        """
