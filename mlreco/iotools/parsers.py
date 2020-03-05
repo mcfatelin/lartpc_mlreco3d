e1dc3a7db0db4764c6ec2b97827cff3f738f3d2a
mcfatelin <mcfatelin@gmail.com>
2020-03-05 14:54:21 -0800

    Add one parser for GAN

--- mlreco/iotools/parsers.py
+++ mlreco/iotools/parsers.py
@@ -353,6 +353,36 @@ def parse_cluster3d_full(data):
     return np_voxels, np_features
 
 
+def parse_cluster3d_reduced(data):
+    """
+    a function to retrieve clusters tensor
+    args:
+        length 2 array of larcv::EventClusterVoxel3D and larcv::EventParticle
+    return:
+        a numpy array with the shape (n,3) where 3 represents (x,y,z)
+        coordinate
+        a numpy array with the shape (n,1) where 1 is voxel value.
+    """
+    cluster_event = data[0]
+    particles_v = data[1].as_vector()
+    meta = cluster_event.meta()
+    num_clusters = cluster_event.as_vector().size()
+    clusters_voxels, clusters_features = [], []
+    for i in range(num_clusters):
+        cluster = cluster_event.as_vector()[i]
+        num_points = cluster.as_vector().size()
+        if num_points > 0:
+            x = np.empty(shape=(num_points,), dtype=np.int32)
+            y = np.empty(shape=(num_points,), dtype=np.int32)
+            z = np.empty(shape=(num_points,), dtype=np.int32)
+            value = np.empty(shape=(num_points,), dtype=np.float32)
+            larcv.as_flat_arrays(cluster,meta,x, y, z, value)
+            clusters_voxels.append(np.stack([x, y, z], axis=1))
+            clusters_features.append(np.column_stack([value]))
+    np_voxels   = np.concatenate(clusters_voxels, axis=0)
+    np_features = np.concatenate(clusters_features, axis=0)
+    return np_voxels, np_features
+
 def parse_cluster3d_full_extended(data):
     '''
     a function to retrieve clusters tensor
